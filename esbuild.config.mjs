35142ced3f0bdaef30fb7f8cdb495c4da9497f58
alex <419725794@qq.com>
2023-12-25 00:58:42 +0800

    build: more info for test environment by dynamicly disabling minify option in esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -75,7 +75,7 @@ const esbuildConfig = (outdir) => ({
 		"cs-search-worker": "src/web-worker/search-worker-server.ts",
 	},
 	bundle: true,
-	minify: true,
+	minify: prod ? true : false,
 	external: [
 		"obsidian",
 		"electron",
