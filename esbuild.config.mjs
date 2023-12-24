31aabfd4f5109d8008b483c820559f3a400eeae1
alex <419725794@qq.com>
2023-12-24 15:46:15 +0800

    fix: can't update via BRAT

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,8 +1,6 @@
 import builtins from "builtin-modules";
 import esbuild from "esbuild";
 import esbuildSvelte from "esbuild-svelte";
-import fsUtil from "fs";
-import pathUtil from "path";
 import process from "process";
 import sveltePreprocess from "svelte-preprocess";
 
@@ -13,60 +11,60 @@ if you want to view the source, please visit the github repository of this plugi
 `;
 const prod = process.argv[2] === "production";
 
-function debounce(delay, func) {
-	let timeoutId;
+// function debounce(delay, func) {
+// 	let timeoutId;
 
-	return (...args) => {
-		clearTimeout(timeoutId);
-		timeoutId = setTimeout(() => func(...args), delay);
-	};
-}
+// 	return (...args) => {
+// 		clearTimeout(timeoutId);
+// 		timeoutId = setTimeout(() => func(...args), delay);
+// 	};
+// }
 
-// 这里会多次使用esbuild进行编译，防止多次同时复制
-let hasCopied = false;
-function copyFile(src, dest) {
-	if (hasCopied) {
-		hasCopied = false;
-		// 跳过偶数次复制
-		return;
-	}
-	hasCopied = true;
-	fsUtil.copyFile(src, dest, (err) => {
-		const formattedTime = new Date().toLocaleTimeString("en-US", {
-			hour12: false,
-			hour: "2-digit",
-			minute: "2-digit",
-			second: "2-digit",
-		});
-		if (err) {
-			console.error(`Error copying ${src}:`, err, `  - ${formattedTime}`);
-		} else {
-			console.log(`Copied ${src} to ${dest}`, `  - ${formattedTime}`);
-		}
-	});
-}
+// // 这里会多次使用esbuild进行编译，防止多次同时复制
+// let hasCopied = false;
+// function copyFile(src, dest) {
+// 	if (hasCopied) {
+// 		hasCopied = false;
+// 		// 跳过偶数次复制
+// 		return;
+// 	}
+// 	hasCopied = true;
+// 	fsUtil.copyFile(src, dest, (err) => {
+// 		const formattedTime = new Date().toLocaleTimeString("en-US", {
+// 			hour12: false,
+// 			hour: "2-digit",
+// 			minute: "2-digit",
+// 			second: "2-digit",
+// 		});
+// 		if (err) {
+// 			console.error(`Error copying ${src}:`, err, `  - ${formattedTime}`);
+// 		} else {
+// 			console.log(`Copied ${src} to ${dest}`, `  - ${formattedTime}`);
+// 		}
+// 	});
+// }
 
-const copyFileDebounced = debounce(1000, copyFile);
+// const copyFileDebounced = debounce(1000, copyFile);
 
-const filesToCopy = ["./assets/styles.css", "./assets/manifest.json"];
-// 监听特定文件的变化
-filesToCopy.forEach((file) => {
-	fsUtil.watch(file, (eventType, filename) => {
-		if (eventType === "change") {
-			copyFileDebounced(file, `./${filename}`);
-		}
-	});
-});
+// const filesToCopy = ["./assets/styles.css", "./assets/manifest.json"];
+// // 监听特定文件的变化
+// filesToCopy.forEach((file) => {
+// 	fsUtil.watch(file, (eventType, filename) => {
+// 		if (eventType === "change") {
+// 			copyFileDebounced(file, `./${filename}`);
+// 		}
+// 	});
+// });
 
-// for the first time `pnpm dev`
-if (!prod) {
-	filesToCopy.forEach((file) => {
-		const destination = `./${pathUtil.basename(file)}`;
-		copyFile(file, destination);
-		// 避免被这个变量影响，导致偶数文件无法复制
-		hasCopied = false;
-	});
-}
+// // for the first time `pnpm dev`
+// if (!prod) {
+// 	filesToCopy.forEach((file) => {
+// 		const destination = `./${pathUtil.basename(file)}`;
+// 		copyFile(file, destination);
+// 		// 避免被这个变量影响，导致偶数文件无法复制
+// 		hasCopied = false;
+// 	});
+// }
 
 const esbuildConfig = (outdir) => ({
 	banner: {
