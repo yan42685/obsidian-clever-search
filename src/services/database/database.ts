0bab6804d07567e572f5a725f907749da1d7a8f4
alex <419725794@qq.com>
2023-12-28 15:19:17 +0800

    feat: use dexie to save pluginSetting fix: error cause by resolving an instance to a static filed
    details:
    Plugin failure: clever-search Error: TypeInfo not known for "e"
        at eval (dependency-container.js:292:27)
        at InternalDependencyContainer.construct (dependency-container.js:297:9)
        at InternalDependencyContainer.resolve (dependency-container.js:114:33)
        at getInstance (my-lib.ts:105:19)
        at new PrivateApi (private-api.ts:10:16)
        at eval (dependency-container.js:289:28)
        at InternalDependencyContainer.construct (dependency-container.js:297:9)
        at InternalDependencyContainer.resolveRegistration (dependency-container.js:166:51)
        at InternalDependencyContainer.resolve (dependency-container.js:109:33)
        at getInstance (my-lib.ts:105:19)

--- src/services/database/database.ts
+++ src/services/database/database.ts
@@ -1,12 +1,88 @@
+import Dexie from "dexie";
 import type { AsPlainObject } from "minisearch";
+import type { PluginSetting } from "src/globals/plugin-setting";
 import type { DocumentRef } from "src/globals/search-types";
+import { logger } from "src/utils/logger";
+import { getInstance } from "src/utils/my-lib";
+import { inject, singleton } from "tsyringe";
+import { PrivateApi } from "../obsidian/private-api";
 
+@singleton()
 export class Database {
+	private readonly db = getInstance(DexieWrapper);
+
 	async getMiniSearchData(): Promise<AsPlainObject | null> {
 		return null;
 	}
 
-    async getDocumentRefs(): Promise<DocumentRef[]> {
-        return [];
-    }
+	async getDocumentRefs(): Promise<DocumentRef[] | null> {
+		return [];
+	}
+
+	async getPluginSetting(): Promise<PluginSetting | null> {
+		try {
+			return (await this.db.pluginSetting.toArray())[0].data;
+		} catch (e) {
+			logger.trace(`database didn't find pluginSetting: ${e}`);
+			return null;
+		}
+	}
+
+	async setPluginSetting(setting: PluginSetting) {
+		this.db
+			.transaction("rw", this.db.pluginSetting, () => {
+				this.db.pluginSetting.clear();
+				this.db.pluginSetting.add({ data: setting });
+			})
+			.then(() => {
+				logger.trace("settings have been saved to database");
+			})
+			.catch((e) => {
+				logger.trace(`settings failed to be saved: ${e}`);
+			});
+	}
+
+	// copied from https://github.com/scambier/obsidian-omnisearch/blob/master/src/database.ts#L36
+	async deleteOldDatabases() {
+		const toDelete = (await indexedDB.databases()).filter(
+			(db) =>
+				db.name === this.db.dbName &&
+				// version multiplied by 10 https://github.com/dexie/Dexie.js/issues/59
+				db.version !== this.db.dbVersion * 10,
+		);
+		if (toDelete.length) {
+			logger.trace("Those IndexedDb databases will be deleted:");
+			for (const db of toDelete) {
+				if (db.name) {
+					indexedDB.deleteDatabase(db.name);
+				}
+			}
+		}
+	}
+}
+
+@singleton()
+class DexieWrapper extends Dexie {
+	private static readonly _dbVersion = 1;
+	private static readonly dbNamePrefix = "clever-search/";
+	private privateApi: PrivateApi;
+	pluginSetting!: Dexie.Table<{ id?: number; data: PluginSetting }, number>;
+	minisearch!: Dexie.Table<{ id?: number; data: AsPlainObject }, number>;
+	documentRefs!: Dexie.Table<{ id?: number; data: DocumentRef[] }, number>;
+
+	constructor(@inject(PrivateApi) privateApi: PrivateApi) {
+		super(DexieWrapper.dbNamePrefix + privateApi.getAppId());
+		this.privateApi = privateApi;
+		this.version(DexieWrapper._dbVersion).stores({
+			pluginSetting: "++id",
+			minisearch: "++id",
+			documentRefs: "path",
+		});
+	}
+	get dbVersion() {
+		return DexieWrapper._dbVersion;
+	}
+	get dbName() {
+		return DexieWrapper.dbNamePrefix + this.privateApi.getAppId();
+	}
 }
