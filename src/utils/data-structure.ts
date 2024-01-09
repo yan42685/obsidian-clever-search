a8673d50c193d32f6d147d23b633869c531d7d4c
alex <419725794@qq.com>
2024-01-09 14:37:31 +0800

    feat(PriorityQueue)

--- src/utils/data-structure.ts
+++ src/utils/data-structure.ts
@@ -1,4 +1,3 @@
-import type { Line, MatchedLine } from "src/globals/search-types";
 import { logger } from "./logger";
 
 export class Collections {
@@ -87,4 +86,116 @@ export class BufferSet<T> {
 			}
 		}
 	}
+}
+
+type Comparator<T> = (a: T, b: T) => number;
+
+// min heap
+export class PriorityQueue<T> {
+    private heap: T[];
+    private compare: Comparator<T>;
+    private capacity: number;
+
+    constructor(
+        comparator: Comparator<T>,
+        capacity: number = Number.MAX_VALUE
+    ) {
+        this.heap = [];
+        this.compare = comparator;
+        this.capacity = capacity;
+    }
+    public values(): T[] {
+        return this.heap;
+    }
+
+    public push(item: T): void {
+        if (this.size() < this.capacity) {
+            this.heap.push(item);
+            this.heapifyUp();
+        } else if (this.compare(item, this.heap[0]) > 0) {
+            this.heap[0] = item;
+            this.heapifyDown();
+        }
+    }
+
+    public pop(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        const item = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        this.heapifyDown();
+        return item;
+    }
+
+    public peek(): T | undefined {
+        return this.heap[0];
+    }
+
+    public size(): number {
+        return this.heap.length;
+    }
+
+    private getLeftChildIndex(parentIndex: number): number {
+        return 2 * parentIndex + 1;
+    }
+
+    private getRightChildIndex(parentIndex: number): number {
+        return 2 * parentIndex + 2;
+    }
+
+    private getParentIndex(childIndex: number): number {
+        return Math.floor((childIndex - 1) / 2);
+    }
+
+    private swap(index1: number, index2: number): void {
+        [this.heap[index1], this.heap[index2]] = [
+            this.heap[index2],
+            this.heap[index1],
+        ];
+    }
+
+    private heapifyUp(): void {
+        let index = this.heap.length - 1;
+        while (
+            this.getParentIndex(index) >= 0 &&
+            this.compare(
+                this.heap[this.getParentIndex(index)],
+                this.heap[index]
+            ) > 0
+        ) {
+            this.swap(this.getParentIndex(index), index);
+            index = this.getParentIndex(index);
+        }
+    }
+
+    private heapifyDown(): void {
+        let index = 0;
+        while (this.getLeftChildIndex(index) < this.heap.length) {
+            let smallerChildIndex = this.getLeftChildIndex(index);
+            if (
+                this.getRightChildIndex(index) < this.heap.length &&
+                this.compare(
+                    this.heap[this.getRightChildIndex(index)],
+                    this.heap[smallerChildIndex]
+                ) < 0
+            ) {
+                smallerChildIndex = this.getRightChildIndex(index);
+            }
+
+            if (
+                this.compare(this.heap[index], this.heap[smallerChildIndex]) < 0
+            ) {
+                break;
+            } else {
+                this.swap(index, smallerChildIndex);
+            }
+            index = smallerChildIndex;
+        }
+    }
+
+    public isEmpty(): boolean {
+        return this.heap.length === 0;
+    }
 }
