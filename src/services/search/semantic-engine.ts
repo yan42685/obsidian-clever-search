b81bf8d3ed66355846d2042020b22144ca1a26bc
alex <419725794@qq.com>
2024-01-28 20:53:49 +0800

    feat(MountedModal): prepare for semantic search

--- src/services/search/semantic-engine.ts
+++ src/services/search/semantic-engine.ts
@@ -1,21 +1,27 @@
-import type { IndexedDocument } from "src/globals/search-types";
+import type { FileItem, IndexedDocument } from "src/globals/search-types";
 import { getInstance } from "src/utils/my-lib";
 import { HttpClient } from "src/utils/web/http-client";
 import { singleton } from "tsyringe";
-import { ViewType } from "../obsidian/view-registry";
+import { ViewRegistry, ViewType } from "../obsidian/view-registry";
 
 @singleton()
 export class SemanticEngine {
 	private request = getInstance(RemoteRequest);
-	async reindexAll(data?: IndexedDocument[]) {
-		const docs = [
-			{
-				path: "abc.txt",
-				view_type: ViewType.MARKDOWN,
-				content: "test content",
-			} as Document,
-		];
+	private viewRegistry = getInstance(ViewRegistry);
+
+	async reindexAll(data: IndexedDocument[]) {
+		const docs = data.map((x) => {
+			return {
+				path: x.path,
+				view_type: this.viewRegistry.viewTypeByPath(x.path),
+				content: x.content,
+			} as Document;
+		});
 		await this.request.reindexAll(docs);
+	}
+
+	async search(queryText: string, viewType: ViewType) {
+		return this.request.search(queryText, viewType);
 	}
 }
 
@@ -33,6 +39,14 @@ class RemoteRequest {
 				vault_id: "test_collection",
 			},
 			docs,
+		);
+	}
+
+	async search(queryText: string, viewType: ViewType): Promise<FileItem[]> {
+		return (
+			JSON.parse(
+				(await this.client.get("search", { queryText, viewType })).json,
+			) || []
 		);
 	}
 }
