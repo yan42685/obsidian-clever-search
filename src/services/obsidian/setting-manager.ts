a4e5a2bb60f3ab3861318a76faff6dbb457ebd08
alex <419725794@qq.com>
2024-03-08 15:18:44 +0800

    chore: update to 0.2.3

--- src/services/obsidian/setting-manager.ts
+++ src/services/obsidian/setting-manager.ts
@@ -492,7 +492,7 @@ class SemanticSearchModal extends Modal {
 	private needInitSemanticEngine = false;
 	onOpen(): void {
 		this.modalEl.style.width = "50vw";
-		this.modalEl.style.height = "40vh";
+		this.modalEl.style.height = "55vh";
 		this.modalEl.querySelector(".modal-close-button")?.remove();
 		const contentEl = this.contentEl;
 		new Setting(contentEl)
