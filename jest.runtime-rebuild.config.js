9b26ec09d7c115733e6bee3f5b1e622cc365c689
alex <419725794@qq.com>
2026-03-22 18:00:14 +0800

    新增本地重建运行时 benchmark

---
+++ jest.runtime-rebuild.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	clearMocks: true,
+	collectCoverage: false,
+	moduleDirectories: ["node_modules", "<rootDir>"],
+	moduleFileExtensions: ["ts", "js"],
+	moduleNameMapper: {
+		"^franc-min$": "<rootDir>/tests/__mocks__/franc-min.js",
+		"^jieba-wasm/pkg/web/jieba_rs_wasm$": "<rootDir>/tests/__mocks__/jieba-wasm-mock.js",
+		"^obsidian$": "<rootDir>/tests/__mocks__/obsidian.js",
+	},
+	roots: ["<rootDir>/tests/src/services/search"],
+	setupFilesAfterEnv: ["<rootDir>/tests/src/jest-setup.js"],
+	testEnvironment: "node",
+	testMatch: ["**/runtime-rebuild.bench.ts"],
+	transform: {
+		"^.+\\.(js|ts)$": [
+			"babel-jest",
+			{
+				presets: [
+					[
+						"@babel/preset-env",
+						{
+							targets: { node: "current" },
+							modules: "commonjs",
+						},
+					],
+					"@babel/preset-typescript",
+				],
+				plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+			},
+		],
+	},
+};
