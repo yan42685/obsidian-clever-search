ec592cb33cbaa1d4e1649b371406e04b543473bd
alex <419725794@qq.com>
2026-04-01 03:39:39 +0800

    修复 RecentFileManager 滚动监听清理

--- src/services/obsidian/user-data/recent-file-manager.ts
+++ src/services/obsidian/user-data/recent-file-manager.ts
@@ -11,11 +11,14 @@ export class RecentFileManager {
 	private plugin: CleverSearch = getInstance(THIS_PLUGIN);
 	private app: App = getInstance(App);
 	private scrollElement: HTMLElement | null = null;
+
 	private handleScroll = () => {
 		const scrollTop = this.scrollElement?.scrollTop || 0;
 		console.log("curr scrollTop:", scrollTop);
-        const scrollInfo: ScrollInfo = {top: scrollTop, left: 0};
+		const scrollInfo: ScrollInfo = { top: scrollTop, left: 0 };
+		void scrollInfo;
 	};
+
 	private handleScrollDebounced = debounce(1000, () => this.handleScroll());
 
 	init() {
@@ -23,33 +26,27 @@ export class RecentFileManager {
 			this.app.workspace.on(
 				"active-leaf-change",
 				async (newLeaf: WorkspaceLeaf | null) => {
-					if (newLeaf && newLeaf.view instanceof MarkdownView) {
-						if (this.scrollElement) {
-							logger.info("remove listener...");
-							this.scrollElement.removeEventListener(
-								"scroll",
-								this.handleScrollDebounced,
-								true,
-							);
-						}
+					this.detachScrollListener();
+					if (!(newLeaf && newLeaf.view instanceof MarkdownView)) {
+						return;
+					}
 
-						// TODO: 处理编辑和阅读模式，类不一样（.cm-scroller)
-						// 阅读模式
-						// this.scrollElement = document.querySelector(".markdown-preview-view");
-						// 编辑模式
-						this.scrollElement =
-							newLeaf.view.containerEl.querySelector(
-								".cm-scroller",
-							);
-						if (this.scrollElement) {
-							logger.info("add listener...");
-							this.scrollElement.addEventListener(
-								"scroll",
-								this.handleScrollDebounced,
-							);
-						} else {
-							logger.error("can't find scrollElement");
-						}
+					// TODO: 澶勭悊缂栬緫鍜岄槄璇绘ā寮忥紝绫讳笉涓€鏍凤紙.cm-scroller)
+					// 闃呰妯″紡
+					// this.scrollElement = document.querySelector(".markdown-preview-view");
+					// 缂栬緫妯″紡
+					this.scrollElement =
+						newLeaf.view.containerEl.querySelector(
+							".cm-scroller",
+						);
+					if (this.scrollElement) {
+						logger.info("add listener...");
+						this.scrollElement.addEventListener(
+							"scroll",
+							this.handleScrollDebounced,
+						);
+					} else {
+						logger.error("can't find scrollElement");
 					}
 				},
 			),
@@ -57,6 +54,18 @@ export class RecentFileManager {
 	}
 
 	onAppQuit() {}
+
+	private detachScrollListener(): void {
+		if (!this.scrollElement) {
+			return;
+		}
+		logger.info("remove listener...");
+		this.scrollElement.removeEventListener(
+			"scroll",
+			this.handleScrollDebounced,
+		);
+		this.scrollElement = null;
+	}
 }
 
 type ScrollInfo = {
